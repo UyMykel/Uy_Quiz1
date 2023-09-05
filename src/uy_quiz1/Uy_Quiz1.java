8e8b4148d76b8a62695a3337e1d13c1d28eebfc7
Student <Student@CLB3Unit06>
2023-09-05 17:32:54 +0800

    FINAL

--- src/uy_quiz1/Uy_Quiz1.java
+++ src/uy_quiz1/Uy_Quiz1.java
@@ -15,7 +15,22 @@ public class Uy_Quiz1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] Array = new int[]{82, 44, 96, 7, 13, 56, 74, 14, 98, 36};
+           for (int i = 0; i <10; i++){
+           System.out.println("Elements of Array");{
+        
+            
+           
+            
+               
+           }
+           
+           
+            
+            
+        
+        }
+            
     }
     
-}
+
